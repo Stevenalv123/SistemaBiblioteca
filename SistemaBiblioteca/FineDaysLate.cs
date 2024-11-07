e437b931c61dc3e7dafb057ba528cfe7f6c148b6
gabriellezama <165319130+gabriellezama@users.noreply.github.com>
2024-11-07 14:15:55 -0600

    #pene loco

---
+++ SistemaBiblioteca/FineDaysLate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblioteca
+{
+    public class FineDaysLate : Fine
+    {
+        public int DaysLate { get; set; }
+
+        private static int counterFines = 1;
+        public FineDaysLate(int daysLate, string iDFine, string iDLoan, decimal baseAmount, decimal totalFine) : base(iDFine, iDLoan, baseAmount, totalFine)
+        {
+            DaysLate = daysLate;
+        }
+        public override string ToString()
+        {
+            return $"RetrasoPorMulta {DaysLate}, IDMulta {IDFine}, IDPrestamo {IDLoan}, MontoBase {BaseAmount}, TotalMulta {TotalFine}";
+        }
+        private string GenerateIdFine()
+        {
+            return $"M{counterFines.ToString("D3")}";
+
+        }
+
+        public static void IncrementCounter()
+        {
+            counterFines++;
+        }
+    }
+}
