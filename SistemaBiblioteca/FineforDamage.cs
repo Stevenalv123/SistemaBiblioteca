522349f18bdac346f964cf375aa73d711b0a4537
Montoya366 <ervin.molina597@std.uni.edu.ni>
2024-11-07 14:27:06 -0600

    #

---
+++ SistemaBiblioteca/FineforDamage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblioteca
+{
+    public class FineforDamage : Fine
+    {
+        
+
+        public string DescriptionOfTheDamage {  get; set; }
+
+
+        private static int counterFines = 1;
+
+        public FineforDamage(string iDFine, string iDLoan, decimal baseAmount, decimal totalFine) : base(iDFine, iDLoan, baseAmount, totalFine)
+        {
+            DescriptionOfTheDamage = DescriptionOfTheDamage;
+
+        }
+        private string GenerateIdFine()
+        {
+            return $"M{counterFines.ToString("D3")}";
+
+        }
+
+        public static void IncrementCounter()
+        {
+            counterFines++;
+        }
+    }
+}
